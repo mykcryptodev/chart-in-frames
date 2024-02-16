9ea169b1edd16663c3b582468242b1ab797b054a
Myk <89474773+mykcryptodev@users.noreply.github.com>
2024-02-15 19:47:12 -0500

    vercel url

--- app/config.ts
+++ app/config.ts
@@ -1,1 +1,1 @@
-export const NEXT_PUBLIC_URL = 'https://059d-2600-4808-8c30-de00-8557-651b-672b-6fe8.ngrok-free.app';
+export const NEXT_PUBLIC_URL = process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : 'https://059d-2600-4808-8c30-de00-8557-651b-672b-6fe8.ngrok-free.app';
