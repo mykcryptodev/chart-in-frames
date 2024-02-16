b215afd4a449cccb567dc18ee9c079d36e53fe53
Myk <89474773+mykcryptodev@users.noreply.github.com>
2024-02-15 19:49:17 -0500

    non auth url

--- app/config.ts
+++ app/config.ts
@@ -1,1 +1,1 @@
-export const NEXT_PUBLIC_URL = process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : 'https://059d-2600-4808-8c30-de00-8557-651b-672b-6fe8.ngrok-free.app';
+export const NEXT_PUBLIC_URL = process.env.NODE_ENV === 'production' ? 'https://chart-in-frames.vercel.app' : 'https://059d-2600-4808-8c30-de00-8557-651b-672b-6fe8.ngrok-free.app';
