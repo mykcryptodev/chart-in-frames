1eea7416adb4cd02ce41555766044c1fc9fcf381
Myk <89474773+mykcryptodev@users.noreply.github.com>
2024-02-15 21:45:09 -0500

    load chart

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Load token chart!',
+      label: 'Load chart!',
     },
     {
       action: 'link',
