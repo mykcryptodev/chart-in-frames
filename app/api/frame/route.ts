5fcc00aa41523662404a99702dc2791a1248bdda
Myk <89474773+mykcryptodev@users.noreply.github.com>
2024-02-15 20:03:41 -0500

    base 64 encode

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -48,8 +48,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     }),
   });
   const chartBlob = await chartRes.blob();
-  const chartUrl = URL.createObjectURL(chartBlob);
-  console.log(chartUrl);
+  const chartBuffer = await chartBlob.arrayBuffer(); // Convert the Blob to an ArrayBuffer
+  const chartBase64 = Buffer.from(chartBuffer).toString('base64'); // Convert the ArrayBuffer to a Base64 string
 
   if (message?.button === 3) {
     return NextResponse.redirect(
@@ -71,7 +71,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       image: {
         // src: `${NEXT_PUBLIC_URL}/park-1.png`,
         // src: `https://quickchart.io/apex-charts/render?config=${JSON.stringify(chartOptions)}`,
-        src: chartUrl,
+        src: chartBase64,
       },
       postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
     }),
